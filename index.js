2e53a7571f59e4aa78fc8be8dacbd1099c471e9a
Space.yg <88850324+Space-yg@users.noreply.github.com>
2023-02-05 19:23:35 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,97 @@
 import * as s from "./seeds.json" assert {type: "json"};
 
+// Initials
+const tbody = document.getElementsByTagName("tbody")[0];
+const resourceFilter = document.getElementById("resourceFilter");
+// Amount Filter
+const amountFilter = document.getElementById("amountFilter");
+const amount = document.getElementById("amount");
+const order = document.getElementById("order");
+// World Size Filter
+const worldSize = document.getElementById("worldSizeFilter");
+const worldSizeCheck = document.getElementById("worldSizeCheck");
+// World Size Filter
+const resourceAmount = document.getElementById("resourceAmountFilter");
+const resourceAmountCheck = document.getElementById("resourceAmountCheck");
+// Other
+const ET = {
+    wd: 7242,
+    s: 5028,
+    i: 7932,
+    cp: 5315,
+    cl: 6954,
+    wl: 3520
+};
+const seeds = s.default.seeds;
+/** @type {HTMLTableRowElement[]} */
+var seedData = [];
+
 // Test any functionality
 function test() { console.log("Test"); }
 
-// Other
-const ET = {
-    wood: 7242,
-    stone: 5028,
-    iron: 7932,
-    copper: 5315,
-    coal: 6954,
-    wolframite: 3520
-};
-const seeds = s.default;
-const tbody = document.getElementsByTagName("tbody")[0];
-var seedData = [];
-
 // Put data in table
-for (const key in seeds) {
+const start = performance.now();
+seeds.forEach(seed => {
     const tr = document.createElement("tr");
     const seedTag = document.createElement("th");
-    seedTag.innerHTML = key;
+    const seedName = Object.keys(seed)[0];
+    seedTag.innerHTML = seedName;
     tr.appendChild(seedTag);
-    var lowestET = seeds[key]["wood"] * 30 / ET["wood"];
-    for (const info in seeds[key]) {
+    // Put Resource data in tr
+    for (const info of ["wd", "s", "i", "cp", "cl", "wl"]) {
         const infoTag = document.createElement("td");
-        infoTag.innerHTML = seeds[key][info];
+        infoTag.innerHTML = seed[seedName][info];
         tr.appendChild(infoTag);
-        (seeds[key][info] * 30 / ET[info] < lowestET) ? lowestET = seeds[key][info] * 30 / ET[info] : lowestET;
     }
-    const td = document.createElement("td");
-    td.innerHTML = Math.round(lowestET * 1000) / 1000;
-    tr.appendChild(td);
+    // World Size
+    const td1 = document.createElement("td");
+    td1.innerHTML = seed[seedName]["ws"];
+    tr.appendChild(td1);
+    // Resource Amount
+    const td2 = document.createElement("td");
+    td2.innerHTML = seed[seedName]["r"];
+    tr.appendChild(td2);
+    // Resource
+    const td3 = document.createElement("td");
+    td3.innerHTML = "0";
+    tr.appendChild(td3);
     seedData.push(tr);
+});
+const end = performance.now();
+console.log(end - start);
+
+/**
+ * Display data in table
+ * @param {HTMLTableRowElement[]} data The data of the seeds
+ * @param {int} showLimit The limit of how many seeds to show
+ */
+function display(data, showLimit = 50) {
+    tbody.innerHTML = "";
+    for (let i = 0; i < data.length && i < showLimit; i++) tbody.appendChild(data[i]);
+    if (data.length >= showLimit) {
+        const tr = document.createElement("tr");
+        const th = document.createElement("th");
+        const button = document.createElement("button");
+        button.setAttribute("class", "showMore");
+        button.addEventListener("click", () => { display(data, showLimit + 50); });
+        button.innerHTML = "Show more";
+        th.setAttribute("colspan", "10");
+        th.style.textAlign = "center";
+        th.appendChild(button);
+        tr.appendChild(th);
+        tbody.appendChild(tr);
+    }
 }
 
 /**
  * Filter
- * @param {String[]} resourceFilter [Resource, Filter] or [`None`]
+ * @param {String[]} resourceFilter [Resource, ">" || "≥" || "=" || "≤" || "<", Amount, "Ascending" || "Descending"] or [Resource, "Max" || "Min"] or ["None"]
  * @param {String} worldSize worldSize.value
  * @param {String} resourceAmount resourceAmount.value
 */
 function filter(resourceFilter, worldSize, resourceAmount) {
     // Add all data to row
-    var rows = [];
-    for (let i = 0; i < seedData.length; i++) rows.push(seedData[i]);
+    var rows = [...seedData];
     
     // Filter World Size
     var size;
@@ -82,76 +129,59 @@ function filter(resourceFilter, worldSize, resourceAmount) {
     else if (resourceFilter[1] === "<") rows = rows.filter(row => Number(row.cells[column].innerHTML) < resourceFilter[2]);
     if (resourceFilter[3] === "Descending") rows.sort(function (a, b) { return b.cells[column].innerHTML - a.cells[column].innerHTML; });
     else if (resourceFilter[3] === "Ascending") rows.sort(function (a, b) { return a.cells[column].innerHTML - b.cells[column].innerHTML; });
-    console.log(resourceFilter);
     // Put data in table
     display(rows);
 }
 
-// Initials
-const resourceFilter = document.getElementById("resourceFilter");
-const amountFilter = document.getElementById("amountFilter");
-const amount = document.getElementById("amount");
-const order = document.getElementById("order");
-const worldSize = document.getElementById("worldSizeFilter");
-const resourceAmount = document.getElementById("resourceAmountFilter");
-
 // Filter function
 function Filter() {
-    // [Resource, ">" || "≥" || "=" || "≤" || "<", Amount, "Ascending" || "Descending"] or [Resource, "Max" || "Min"] or [`None`]
+    // [Resource, ">" || "≥" || "=" || "≤" || "<", Amount, "Ascending" || "Descending"] or [Resource, "Max" || "Min"] or ["None"]
     var resource = (resourceFilter.selectedIndex) ? (amountFilter.selectedIndex >= 2) ? [resourceFilter.options[resourceFilter.selectedIndex].text, amountFilter.options[amountFilter.selectedIndex].text, Number(amount.value), order.value] : [resourceFilter.options[resourceFilter.selectedIndex].text, amountFilter.options[amountFilter.selectedIndex].text] : ["None"];
-    if (document.getElementById("worldSizeNone").checked) {
-        if (document.getElementById("resourceAmountNone").checked) filter(resource, "None", "None");
-        else filter(resource, "None", resourceAmount.value);
+    if (worldSizeCheck.checked) {
+        if (resourceAmountCheck.checked) filter(resource, worldSize.value, resourceAmount.value);
+        else filter(resource, worldSize.value, "None");
     } else {
-        if (document.getElementById("resourceAmountNone").checked) filter(resource, worldSize.value, "None");
-        else filter(resource, worldSize.value, resourceAmount.value);
+        if (resourceAmountCheck.checked) filter(resource, "None", resourceAmount.value);
+        else filter(resource, "None", "None");
     }
 }
-Filter();
 
-// Resource Filter
-resourceFilter.addEventListener("change", Filter);
-
+function calculateResources() {
+    console.log();
+    if (Item.items[resourceFilter.value] !== undefined) {
+        /** @type {Item} */
+        const item = Item.items[resourceFilter.value];
+        seedData.forEach(tr => tr.children.item(9).innerHTML = Math.round(item.getMaxResourceAmountInSeed({
+            woodLog: parseInt(tr.children.item(1).innerHTML),
+            stone: parseInt(tr.children.item(2).innerHTML),
+            ironOre: parseInt(tr.children.item(3).innerHTML),
+            copperOre: parseInt(tr.children.item(4).innerHTML),
+            coal: parseInt(tr.children.item(5).innerHTML),
+            wolframite: parseInt(tr.children.item(6).innerHTML)
+        }) * 1000) / 1000);
+    } else seedData.forEach(tr => tr.children.item(9).innerHTML = "0");
+    Filter();
+}
+resourceFilter.addEventListener("change", calculateResources);
 // Amount Filter
 amountFilter.addEventListener("change", Filter);
+amount.addEventListener("change", Filter);
+order.addEventListener("change", Filter);
+// World Size Filter
+worldSize.addEventListener("mouseup", Filter);
+worldSizeCheck.addEventListener("click", Filter);
+// Resource Amount
+resourceAmount.addEventListener("mouseup", Filter);
+resourceAmountCheck.addEventListener("click", Filter);
+Filter();
+
 function amountFunction() {
     if (amountFilter.selectedIndex >= 2) {
-        amount.style.visibility = "visible";
-        order.style.visibility = "visible";
+        amount.style.display = "inline-block";
+        order.style.display = "inline-block";
     } else {
-        amount.style.visibility = "hidden";
-        order.style.visibility = "hidden";
+        amount.style.display = "none";
+        order.style.display = "none";
     }
 }
 amountFilter.addEventListener("change", amountFunction);
-amount.addEventListener("change", Filter);
-order.addEventListener("change", Filter);
-
-// World Size Filter
-worldSize.addEventListener("mouseup", Filter);
-document.getElementById("worldSizeNone").addEventListener("click", Filter);
-document.getElementById("worldSizeNum").addEventListener("click", Filter);
-
-// Resource Amount
-resourceAmount.addEventListener("mouseup", Filter);
-document.getElementById("resourceAmountNone").addEventListener("click", Filter);
-document.getElementById("resourceAmountNum").addEventListener("click", Filter);
-
-// Display data in table
-function display(data, showLimit = 50) {
-    tbody.innerHTML = "";
-    for (let i = 0; i < data.length && i < showLimit; i++) tbody.appendChild(data[i]);
-    if (data.length >= showLimit) {
-        const tr = document.createElement("tr");
-        const th = document.createElement("th");
-        const button = document.createElement("button");
-        button.setAttribute("class", "showMore");
-        button.addEventListener("click", () => { display(data, showLimit + 50); });
-        button.innerHTML = "Show more";
-        th.setAttribute("colspan", "10");
-        th.style.textAlign = "center";
-        th.appendChild(button);
-        tr.appendChild(th);
-        tbody.appendChild(tr);
-    }
-}
