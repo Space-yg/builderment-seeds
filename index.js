b2ffcbbcbd2a49678a1d48edbe4adf8562c0d1ba
Space.yg <88850324+Space-yg@users.noreply.github.com>
2023-10-09 16:54:36 +0300

    Updated the Footer to Redirect to the Builderment server rather than the Vault

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const file = await fetch(path);
 const text = await file.text();
 const d = document.createElement("div");
 d.innerHTML = `<footer>
-<p>For any inquiries, ask on <a href="https://discord.gg/U8SgSsRFKM" target="_blank">The Builderment Vault</a> discord server :D</p>
+<p>For any inquiries, ask on <a href="https://discord.gg/builderment-618226575652945941" target="_blank">Builderment</a> discord server :D</p>
 <p><a href="${path}">${new DOMParser().parseFromString(text, 'text/html').getElementsByTagName("h2").item(0).innerHTML}</a></p>
 </footer>`;
 document.body.appendChild(d.firstChild);
